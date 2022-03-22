47eae031306bd0961125876a253540075215d2c6
esysolina <evelina.ova@gmail.com>
2022-03-22 23:54:40 +0500

    initial commit

---
+++ src/test/java/sysolina/e/tests/LitresTests.java
@@ -0,0 +1,109 @@
+package sysolina.e.tests;
+
+import com.codeborne.selenide.Condition;
+import sysolina.e.helpers.DriverUtils;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class LitresTests extends TestBase {
+    @Test
+    @Description("Тест поисковой строки")
+    @DisplayName("Успешный поиск автора")
+    void generatedTest() {
+        step("Открыть 'https://www.litres.ru/'", () ->
+                open("https://www.litres.ru/"));
+
+        step("Ввести в поисковую строку 'Акунин'", () -> {
+            $(".Search-module__input").setValue("Акунин");
+        });
+
+        step("Сделать элемент видимым", () -> {
+            $(".Search-module__input").shouldBe(visible);
+        });
+
+        step("Нажать кнопку поиска", () -> {
+            $(".Search-module__button").click();
+        });
+
+        step("Проверить наличие в выдаче 'Акунин'", () -> {
+            $(".search__found").shouldHave(Condition.text("Акунин"));
+        });
+    }
+
+    @Test
+    @Description("Тест заголовка")
+    @DisplayName("Проверка заголовка на содержание текста")
+    void titleTest() {
+        step("Открыть 'https://www.litres.ru/'", () ->
+                open("https://www.litres.ru/"));
+
+        step("Заголовок страницы должен содержать текст 'ЛитРес – библиотека электронных книг, скачать в fb2 или читать онлайн на LitRes'", () -> {
+            String expectedTitle = "ЛитРес – библиотека электронных книг, скачать в fb2 или читать онлайн на LitRes";
+            String actualTitle = title();
+
+            assertThat(actualTitle).isEqualTo(expectedTitle);
+        });
+    }
+
+    @Test
+    @Description("Тест на ошибки в консоли")
+    @DisplayName("В логах страницы не должно быть ошибок")
+    void consoleShouldNotHaveErrorsTest() {
+        step("Открыть 'https://www.litres.ru/'", () ->
+                open("https://www.litres.ru/"));
+
+        step("Console logs should not contain text 'SEVERE'", () -> {
+            String consoleLogs = DriverUtils.getConsoleLogs();
+            String errorText = "SEVERE";
+
+            assertThat(consoleLogs).doesNotContain(errorText);
+        });
+    }
+
+    @Test
+    @Description("Тест выдачи жанров в поиске")
+    @DisplayName("В логах страницы не должно быть ошибок")
+    void genreTest(String testData, String expectedResult) {
+        step("Открыть 'https://www.litres.ru/'", () ->
+                open("https://www.litres.ru/"));
+
+        step("Ввести в поисковую строку 'Цвейг'", () -> {
+            $(".Search-module__input").setValue("Цвейг");
+        });
+
+        step("Нажать кнопку поиска", () -> {
+            $(".Search-module__button").click();
+        });
+
+        step("Проверить наличие в выдаче жанра", () -> {
+            $(".rmd-genre-href").shouldHave(Condition.text("зарубежная классика"));
+        });
+    }
+
+    @Test
+    @Description("Тест выдачи авторов по названию в поиске")
+    @DisplayName("В логах страницы не должно быть ошибок")
+    void nameTest(String testsData, String expectedResults) {
+        step("Открыть 'https://www.litres.ru/'", () ->
+                open("https://www.litres.ru/"));
+
+        step("Ввести в поисковую строку 'Сто лет одиночества'", () -> {
+            $(".Search-module__input").setValue("Сто лет одиночества");
+        });
+
+        step("Нажать кнопку поиска", () -> {
+            $(".Search-module__button").click();
+        });
+
+        step("Проверить в выдаче наличие автора 'Маркес'", () -> {
+            $(".art-item__author_label").shouldHave(Condition.text("Габриэль Гарсиа Маркес"));
+        });
+    }
+}
---
+++ src/test/java/sysolina/e/tests/TestBase.java
@@ -0,0 +1,39 @@
+package sysolina.e.tests;
+
+import sysolina.e.config.Project;
+import sysolina.e.helpers.AllureAttachments;
+import sysolina.e.helpers.DriverSettings;
+import sysolina.e.helpers.DriverUtils;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.junit5.AllureJunit5;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+
+@ExtendWith({AllureJunit5.class})
+public class TestBase {
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DriverSettings.configure();
+    }
+
+    @AfterEach
+    public void addAttachments() {
+        String sessionId = DriverUtils.getSessionId();
+
+        AllureAttachments.addScreenshotAs("Last screenshot");
+        AllureAttachments.addPageSource();
+//        AllureAttachments.attachNetwork(); // todo
+        AllureAttachments.addBrowserConsoleLogs();
+
+        Selenide.closeWebDriver();
+
+        if (Project.isVideoOn()) {
+            AllureAttachments.addVideo(sessionId);
+        }
+    }
+}
